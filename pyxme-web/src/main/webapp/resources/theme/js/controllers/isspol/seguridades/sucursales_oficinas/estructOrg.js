e27929a89706245f88c09299e7882f454357c7c1
Santiago Yacelga <esyacelga@hotmail.com>
2017-03-16 16:31:58 -0500

    Menu estrucutura organizacional

---
+++ pyxme-web/src/main/webapp/resources/theme/js/controllers/isspol/seguridades/sucursales_oficinas/estructOrg.js
@@ -0,0 +1,11 @@
+
+app.controller("EstructOrg",['ngNotify',"$scope", 'ngTableParams', 
+						  function( ngNotify,  $scope , ngTableParams) {
+	 
+	  controller = this;
+	  controller.lstSuc= [];
+	  
+	  
+	
+	  
+}]);
