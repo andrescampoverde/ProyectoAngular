af6cb05fb627d7c258e51ad0d0f2070df811bdbb
greenintegrationdesarrollo <afcampoverderengel@gmail.com>
2017-03-17 10:11:34 -0500

    nivel organizacion

---
+++ pyxme-web/src/main/webapp/resources/theme/js/controllers/isspol/seguridades/usuarios_perfiles/usuarioPerfil.js
@@ -0,0 +1,9 @@
+/**
+ * Created by andres.campoverde on 17/03/2017.
+ */
+app.controller('UsuariosPerfiles',['ngNotify',"$scope", 'ngTableParams',
+    function (ngNotify,$scope,ngTableParams) {
+        controller=this;
+        controller
+    }
+]);
